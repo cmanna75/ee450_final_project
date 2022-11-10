23e3f09ba015d9b3e8181590823654452e1acec2
cmanna75 <cmanna@usc.edu>
2022-11-09 20:52:34 -0800

    getsockname stuff

--- client.cpp
+++ client.cpp
@@ -16,15 +16,21 @@ int main(){
     //create tcp socket to communicate with serverM, (IPv4, TCP, IP)
     int client_socket = socket(AF_INET,SOCK_STREAM,0);
 
-    //address data structure for bind
+    //server address
     struct sockaddr_in server_address;
     server_address.sin_family  = AF_INET;
     server_address.sin_addr.s_addr = inet_addr("127.0.0.1"); 
     server_address.sin_port = htons(SERVER_PORT); // htons converts to network byte order(big endian)
 
+    //client address
+    struct sockaddr_in client_address;
+    socklen_t client_length = sizeof(client_address);
+
+
     //connect to host
     connect(client_socket, (struct sockaddr*)&server_address,sizeof(server_address));
-
+    getsockname(client_socket,(struct sockaddr*)&client_address,&client_length);
+    printf("Port no: %u", ntohs(client_address.sin_port));
     close(client_socket);
     return 0;
 }
