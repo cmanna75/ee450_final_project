dfae10b2c767c002f0233753348267b5f93810cd
cmanna75 <cmanna@usc.edu>
2022-11-23 09:40:45 -0800

    basic course lookup needs to be cleaned up and imp into server

--- serverEE.cpp
+++ serverEE.cpp
@@ -6,6 +6,50 @@ using namespace std;
 
 //socket defintion
 int udp_socket;
+
+string search_course(string message){
+    ifstream courses("ee.txt");
+    string course_info;
+    if(courses.is_open()){
+        while(getline(courses,course_info)){
+            //if class codes match look for specified category
+            if(course_info.substr(0,5) == message.substr(0,5)){
+                string ctg = message.substr(6,message.length()-6);
+                int i = 0;
+                if(ctg == "Credit"){
+                    i = 1;
+                }
+                else if(ctg == "Professor"){
+                    i = 2;
+                }
+                else if(ctg == "Days"){
+                    i = 3;
+                }
+                else if(ctg == "CourseName"){
+                    i  = 4;
+                }
+                int j = 5;
+                int start, end;
+                while(i >= 0){
+                   //printf("%c, %i\n",course_info[j], i);
+                    if(course_info[j] == ','||course_info[j] == '\0'){
+                        if(i == 1)
+                            start = j+1;
+                        else if(i == 0){
+                            end = j-1;
+                        }
+                        i--;
+                    }
+                    j++;
+                }
+                string msg_out = "The " +ctg+" of "+message.substr(0,5)+" is "+course_info.substr(start,end-start+1)+".";
+                return msg_out;
+            } 
+        }         
+    }
+    string msg_out = "Didn’t find the course: " + message.substr(0,5);
+    return msg_out;
+}
 void interrupt_handler(int signal){
     printf("Closing Socket");
     close(udp_socket);
@@ -13,7 +57,7 @@ void interrupt_handler(int signal){
 }
 
 int main(){
-
+    /*
     //handle cntrl^c
     signal(SIGINT, interrupt_handler);
 
@@ -34,15 +78,15 @@ int main(){
     while(1){
         //wait for credentials
         n = recvfrom(udp_socket,buffer,102,0,(struct sockaddr *) &client_address, &client_length);
-        /*
         string message(buffer);
-
         printf("The ServerEE received an authentication request from the Main Server\n");
-        //check if crednetials are valid, and send to main server
+        
+        string msg_out = search_course(message);
         sendto(udp_socket,&response,1,0,(struct sockaddr *) &client_address, client_length);
         //send response
         printf("The ServerC finished sending the response to the Main Server.\n");
         */
-    }
+    search_course("EE520,CourseName");
+    //}
     return 0;
 }
