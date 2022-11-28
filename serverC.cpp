3c85f42f1c5c267bc933413c68955102061e188f
cmanna75 <cmanna@usc.edu>
2022-11-28 12:13:22 -0800

    cleaned up serverC

--- serverC.cpp
+++ serverC.cpp
@@ -8,14 +8,9 @@ char check_credentials(string message){
     ifstream creds("cred.txt");
     string enc;
     char flag = FAIL_NO_USR;
-    //printf("%s\n", message.c_str());
     if(creds.is_open()){
         //compare each string
         while(getline(creds,enc)){
-            //remove delimtter
-            //enc.erase(enc.size()-1,1);
-            //printf("%s\n",enc.c_str());
-            //compare, //if username is correct set flag to 1
             int i = 0;
             while( (i < message.length()) && (i < enc.length())){
                 //if mismatch do not check rest of string
@@ -27,6 +22,7 @@ char check_credentials(string message){
                 //username is correct
                 else if(message[i] == ',')
                     flag = FAIL_NO_PASS;
+                //password is correct
                 else if( (i == message.length()-1) && i == enc.length()-1)
                     return PASS_CRED;
                 i++;
@@ -56,14 +52,13 @@ int main(){
     struct sockaddr_in client_address;
     memset(&client_address, 0, sizeof(client_address));
     socklen_t client_length = sizeof(client_address);
-    int n;
 
     //booting up message
     printf("serverC is up and running using UDP on port %i\n", UDP_PORT);
     while(1){
         //wait for credentials
         memset(buffer,0,102);
-        n = recvfrom(udp_socket,buffer,102,0,(struct sockaddr *) &client_address, &client_length);
+        recvfrom(udp_socket,buffer,102,0,(struct sockaddr *) &client_address, &client_length);
         string message(buffer);
         //printf("message: %s\n",message.c_str());
         printf("The ServerC received an authentication request from the Main Server\n");
@@ -74,4 +69,5 @@ int main(){
         //send response
         printf("The ServerC finished sending the response to the Main Server.\n");
     }
+    return 0;
 }
